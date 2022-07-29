62f541c2653c5403c37440a7d49cc002e6fad7fe
Ali Krenbi <krimbzzz@gmail.com>
2022-07-29 23:55:42 +0300

    homework7

---
+++ homework7/clean_folder/clean_folder/main.py
@@ -0,0 +1,19 @@
+import os
+import sys
+from clean import arrange_folder
+
+
+def main():
+    try:
+        target_folder = sys.argv[1]
+    except IndexError:
+        print("Target folder doesn't defined!")
+        return
+    if not os.path.exists(target_folder):
+        print("Bad path")
+        return
+    arrange_folder(target_folder, target_folder)
+
+
+if __name__ == "__main__":
+    main()
--- homework7/clean_folder/setup.py
+++ homework7/clean_folder/setup.py
@@ -1,12 +1,11 @@
-from setuptools import setup, find_namespace_packages
+from setuptools import setup, find_packages
 
 setup(
     name='clean_folder',
     version='0.0.1',
-    description='Clean folder script',
     author='Ali Krenbi',
-
-    #packages=find_packages(),
     entry_points = {
-            'console_scripts': ['clean=clean_folder.cleaner:main']},
+            'console_scripts': ['clean=clean_folder.main:main']},
+    packages=find_packages(),
+    description="Clean folder script"
 )
